e4f93c2b9edb27a5c359df95d898e0d8e12adc54
marcomarcucci30 <marco.marcucci96@gmail.com>
2020-09-26 15:57:11 +0200

    Update test

--- test/test_tolerance.py
+++ test/test_tolerance.py
@@ -10,13 +10,19 @@ def test():
     client_as = boto3.client('autoscaling')
 
     response = client_as.describe_auto_scaling_instances()
+    """list_a = []
+    
+    for instance in response['AutoScalingInstances']:
+        if instance['AutoScalingGroupName'] == 'areaAasg':
+            list_a.append(instance)"""
+
     print("Number of instance before deleting: ", len(response['AutoScalingInstances']))
     id_instance = response['AutoScalingInstances'][0]['InstanceId']
     print("Deleting instance...")
     ec2.instances.filter(InstanceIds=id_instance).terminate()
 
     response = client_as.describe_auto_scaling_instances()
-    while len(response['AutoScalingInstances']) == 2:
+    while len(response['AutoScalingInstances']) == 6:
         print("Deleting instance...")
         sleep(5)
         response = client_as.describe_auto_scaling_instances()
@@ -25,7 +31,7 @@ def test():
     print("Number of instances after deleting:", len(response['AutoScalingInstances']))
 
     response = client_as.describe_auto_scaling_instances()
-    while len(response['AutoScalingInstances']) == 1:
+    while len(response['AutoScalingInstances']) == 5:
         print("Waiting autoscaler create instance...")
         sleep(5)
         response = client_as.describe_auto_scaling_instances()
